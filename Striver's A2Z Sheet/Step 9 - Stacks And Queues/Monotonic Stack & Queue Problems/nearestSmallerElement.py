99dcae92fc5768b73863eaf1b1da2c9ef80686b1
Rohit Chawhan <rohitchawhan555@gmail.com>
2025-04-17 09:05:45 +0530

    Next Greater Element, Next Greater Element II and Nearest Smaller Element Completed

--- Striver's A2Z Sheet/Step 9 - Stacks And Queues/Monotonic Stack & Queue Problems/nearestSmallerElement.py
+++ Striver's A2Z Sheet/Step 9 - Stacks And Queues/Monotonic Stack & Queue Problems/nearestSmallerElement.py
@@ -20,45 +20,36 @@ from collections import deque
 from collections import Counter
 
 class Solution:
-  def findSolution1(self, arr):
+  def findSolution(self, arr):
     stack = LifoQueue()
     ans = [0 for i in range(len(arr))] 
-    for i in range(len(arr)-1, -1, -1):
-      while(not stack.empty() and arr[i] > stack.queue[-1]):
+    for i in range(0, len(arr)):
+      while(not stack.empty() and arr[i] <= stack.queue[-1]):
         stack.get()
       ans[i] = -1 if stack.empty() else stack.queue[-1]
       stack.put(arr[i])
     return ans
-  
-  def findSolution(self, arr):
-    stack = LifoQueue()
-    ans = {} 
-    for i in range(len(arr)-1, -1, -1):
-        while(not stack.empty() and arr[i] >= stack.queue[-1]):
-            stack.get()
-        ans[arr[i]] = -1 if stack.empty() else stack.queue[-1]
-        stack.put(arr[i])
-    
-    return ans
+
 
 if __name__ == "__main__":
-  arr = [2, 4, 6, 7, 5, 1, 0]
-  arr = [1, 2, 3, 4, 5, 6, 7]
+  # arr = [2, 4, 6, 7, 5, 1, 0]
+  # arr = [1, 2, 3, 4, 5, 6, 7]
+  arr = [ 39, 27, 11, 4, 24, 32, 32, 1 ]
   print(Solution().findSolution(arr))
 
 """
-Next Greater Element
+Next Smaller Element
 Brute Force
-1. Run a loop over the array and for every element run another loop till next greater element is found.
+1. Run a loop over the array and for every element run another loop till next previous smaller element is found.
 2. If found add to answer and break loop else add -1 to ans array.
 3. Return the final ans array
 TC -> O(N**2), SC -> O(n)
 
 Optimal Approach
-1. Iterate over the array in reverse order
-2. While Stack is not empty and element is greater than stack top pop stack elements
+1. Iterate over the array
+2. While Stack is not empty and element is smaller than stack top pop stack elements
 3. If stack is empty add -1 to ans array else add stack top to ans array
 4. Push the current element to stack
-5. Return the ans array in reverse order
+5. Return the ans array
 TC -> O((N + N/2) * log N), SC -> O(2n)
 """
